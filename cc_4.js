1b5cb87a709c7daa75d073c83660932130ca42d1
Sara Youssef <https://github.com/SaraYoussef26/Portfolio-site.git>
2025-09-18 08:55:01 -0400

    Created an array

---
+++ cc_4.js
@@ -0,0 +1,10 @@
+//Coding Challenge 4
+
+//Step 2: Creating an array
+let products = [
+    {name: "Cell Phone", category: "electronics",  price: 600,  inventory: 8},
+    {name: "Strawberry", category: "groceries",  price: 3.12,  inventory: 20},
+    {name: "Paper Towels", category: "household",  price: 6.03,  inventory: 15},
+    {name: "Skirt", category: "apparel",  price: 19.99,  inventory:8},
+    {name: "Pencil", category: "other", price: 12.10,  inventory: 10},
+];
